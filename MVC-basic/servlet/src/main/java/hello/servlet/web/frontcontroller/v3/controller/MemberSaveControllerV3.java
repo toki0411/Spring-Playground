222afacc81ce40755303ce2dc0663b616f270efe
toki0411 <toki402@naver.com>
2023-05-17 15:57:02 +0900

    feat: 프론트 컨트롤러 도입 v3 - 회원 저장

---
+++ MVC-basic/servlet/src/main/java/hello/servlet/web/frontcontroller/v3/controller/MemberSaveControllerV3.java
@@ -0,0 +1,25 @@
+package hello.servlet.web.frontcontroller.v3.controller;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.v3.ControllerV3;
+
+import java.util.Map;
+
+public class MemberSaveControllerV3 implements ControllerV3 {
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public ModelView process(Map<String, String> paramMap) {
+        String username = paramMap.get("username");
+        int age = Integer.parseInt(paramMap.get("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        ModelView modelView = new ModelView("save-result");  //jsp 이름, 현재 링크는 /save
+        modelView.getModel().put("member", member);  //hashmap에 추가
+        return modelView;
+    }
+}
