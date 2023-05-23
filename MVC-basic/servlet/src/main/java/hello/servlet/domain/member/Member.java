fc2a835cceef1f9f565b655b72192324778e75d0
toki0411 <toki402@naver.com>
2023-05-23 11:08:57 +0900

    feat: 멤버 객체

---
+++ MVC-basic/servlet/src/main/java/hello/servlet/domain/member/Member.java
@@ -0,0 +1,20 @@
+package hello.servlet.domain.member;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Member {
+
+    private Long id;
+    private String username;
+    private int age;
+
+    public Member(){  //기본 생성자
+
+    }
+    public Member(String username, int age){
+        this.username = username;
+        this.age = age;
+    }
+}
