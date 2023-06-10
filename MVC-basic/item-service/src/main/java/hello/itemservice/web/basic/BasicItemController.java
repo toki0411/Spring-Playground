d61a573f79f1b0b62dd45cdf70be47275d5b0d86
toki0411 <toki402@naver.com>
2023-06-10 14:33:27 +0900

    feat: 상품 목록 컨트롤러

---
+++ MVC-basic/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,48 @@
+package hello.itemservice.web.basic;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+    private final ItemRepository itemRepository;
+
+//    @Autowired  //@RequiredArgsConstructor가 있으면 주석처리한 코드들 안 써도 됨
+//    public BasicItemController(ItemRepository itemRepository){  //생성자에 스프링 빈 주입
+//        this.itemRepository = itemRepository;
+//    }
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items",items);
+        return "basic/items";//뷰가 있어야함 뷰를 찾아서 랜더링
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model){
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item",item);
+        return "basic/item";
+    }
+
+
+    //테스트용 데이터 추가
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
+    }
+
+}
