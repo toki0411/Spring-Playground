d3ab299590404e36572ba81670b20f847ecae2f3
toki0411 <toki402@naver.com>
2023-06-16 00:28:45 +0900

    feat: 회원 엔티티

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,28 @@
+package jpabook.jpashop.domain;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member")  //아래의 것은 매핑되는 것임. 읽기전용이 됨
+    private List<Order> orders = new ArrayList<>();
+}
