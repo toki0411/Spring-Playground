b2328f6a6be94f17d4bb8df22b71f796e53afc56
toki0411 <toki402@naver.com>
2023-06-20 11:20:49 +0900

    feat: 주문상품 엔티티

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -0,0 +1,98 @@
+package jpabook.jpashop.domain;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "order_item")
+public class OrderItem {
+
+    @Id
+    @GeneratedValue
+    @Column(name="order_item_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    private int orderPrice;  //주문 가격
+    private int count;  //주문 수량
+
+    //==생성 메서드==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {  //주문 상품 엔티티 생성
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);  //주문의 수량만큼 재고 삭제
+        return orderItem;
+    }
+
+    //==비즈니스 로직==//
+    //주문 취소
+    public void cancel(){
+        getItem().addStock(count);
+    }
+
+    //==조회 로직==//
+    //주문상품 전체 가격 조회
+    public int getTotalPrice(){
+        return getOrderPrice() * getCount();
+    }
+}
+//
+//@Entity
+//@Table(name = "order_item")
+//@NoArgsConstructor(access= AccessLevel.PROTECTED)
+//@Getter
+//@Setter
+//public class OrderItem {
+//
+//    @Id
+//    @GeneratedValue
+//    @Column(name="order_item_id")
+//    private Long id;
+//
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name="item_id")
+//    private Item item;
+//
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name="order_id")
+//    private Order order;
+//
+//    private int orderPrice;  //주문 가격
+//    private int count;  //주문 수량
+//
+//    //==생성 메서드==//
+//    public static OrderItem createOrderItem(Item item, int orderPrice, int count){
+//        OrderItem orderItem = new OrderItem();
+//        orderItem.setItem(item);
+//        orderItem.setOrderPrice(orderPrice);
+//        orderItem.setCount(count);
+//
+//        item.removeStock(count);
+//        return orderItem;
+//    }
+//    //==비즈니스 로직==//
+//    public void cancel() {
+//        getItem().addStock(count);
+//    }
+//
+//    public int getTotalPrice() {
+//        return getOrderPrice() * getCount();
+//    }
+//}
